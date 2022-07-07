0893ca334643a2e4b70b7d81cc19d1a0993f76a8
heokyeongju <beewt@naver.com>
2022-07-07 15:41:11 +0900

    등록 êµ기본 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -14,6 +14,18 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             switch (cmd) {
+                case "등록":
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine().trim();
+                    System.out.println("번 명언이 등록되었습니다.");
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    break;
+                case "삭제":
+                    break;
                 case "종료":
                     break outer;
             }
