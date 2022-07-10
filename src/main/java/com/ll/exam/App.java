8f68fb87ec84701677bf86ae3b641250e6cd927d
heokyeongju <beewt@naver.com>
2022-07-10 19:04:43 +0900

    json-simple 의존성 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,11 +1,19 @@
 package com.ll.exam;
 
 import java.util.Scanner;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
         int wiseSayingLastId = 0; //가장 마지막 명언글의 번호
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         outer:
@@ -20,13 +28,19 @@ public class App {
                     System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
                     int id = ++wiseSayingLastId;
+
                     WiseSaying wiseSaying = new WiseSaying(id, content,author);
-                    System.out.println(wiseSaying);
+                    wiseSayings.add(wiseSaying);
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n",id);
                     break;
                 case "목록":
                     System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-----------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
                     break;
                 case "삭제":
                     break;
